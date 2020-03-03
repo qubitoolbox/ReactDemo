da49b432386c1d897ae7195958aad25df215daed
qubitoolbox <osmanmorales@ymail.com>
2020-03-02 23:51:28 -0500

    fix profiletab

--- src/assets/jss/material-kit-react/views/profilePage.js
+++ src/assets/jss/material-kit-react/views/profilePage.js
@@ -1,6 +1,6 @@
-import { container, title } from "C:/NewReactResume/resm/src/assets/jss/material-kit-react.js";
+import { container, title } from "../../material-kit-react.js";
 
-import imagesStyle from "C:/NewReactResume/resm/src/assets/jss/material-kit-react/imagesStyles.js";
+import imagesStyle from "../../material-kit-react/imagesStyles.js";
 
 const profilePageStyle = {
   container,
