c0f87992335963134b06051d1d29ea0d9a00b4f5
qubitoolbox <osmanmorales@ymail.com>
2020-03-20 17:20:57 -0400

    maint

---
+++ src/Components/Maint/maint.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import {Container, Box, Paper, Typography} from '@material-ui/core/'
+import {makeStyles} from '@material-ui/styles/'
+import { blueGrey, brown } from '@material-ui/core/colors/';
+
+const useStyles = makeStyles(theme => ({
+    
+    bgaColor:{
+        height: "90vh",
+        maxHeight: "1000px",
+        overflow: "hidden",
+        position: "relative",
+        backgroundPosition: "center center",
+        backgroundSize: "cover",
+        backgroundColor: "green",
+        margin: "0",
+        padding: "0",
+        border: "0",
+        display: "flex",
+        alignItems: "center"
+    }
+    
+    
+  }));
+
+
+export default function Maint(){
+const classes = useStyles()
+    return(<div >
+        <Box >
+            <Typography>This page is under construction</Typography>
+            
+        </Box>
+
+    </div>)
+
+
+}
